8368ad11c933ca8391de9bb9b8429509ffcf600d
Daniel Granerud <daniel.granerud@payex.com>
2021-01-27 12:12:29 +0100

    fix: Metadata declaration

--- src/models/MetaData.d.ts
+++ src/models/MetaData.d.ts
@@ -1,5 +1,5 @@
 import { GenericResource } from "./GenericResource";
 
 export interface MetaData extends GenericResource {
-
+    [property: string]: any //eslint-disable  @typescript-eslint/no-explicit-any
 }
