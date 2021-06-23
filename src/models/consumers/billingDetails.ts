9c9365f46ccd6f9fb998d77996f8cf6a824c1cf8
Daniel Granerud <42067409+NullableInt@users.noreply.github.com>
2021-06-23 12:53:57 +0200

    Feat: Added consumers models

---
+++ src/models/consumers/billingDetails.ts
@@ -0,0 +1,5 @@
+export interface BillingDetails {
+    billingAddress: Address;
+    email: EmailAddress;
+    msisdn: Msisdn;
+}
---
+++ src/models/consumers/shippingDetails.ts
@@ -0,0 +1,5 @@
+export interface ShippingDetails {
+    email: EmailAddress;
+    msisdn: Msisdn;
+    shippingAddress: Address;
+}
