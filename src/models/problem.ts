cacb075bb1f32f6243fcea508120f26255c8546a
Xillians <chkxillians@gmail.com>
2022-01-26 11:39:45 +0100

    extend basemodel on problem

--- src/models/problem.ts
+++ src/models/problem.ts
@@ -1,11 +1,29 @@
 import { ProblemItem } from "./problemItem";
+import * as v from 'class-validator';
+import { Type } from 'class-transformer';
+import { BaseModel } from './generics/baseModel';
 
-export interface Problem {
+export class Problem extends BaseModel {
+    @v.IsString()
     action: string;
+
+    @v.IsString()
     detail: string;
+
+    @v.IsString()
     instance: string;
+
+    @v.IsArray()
+    @v.ValidateNested()
+    @Type(() => ProblemItem)
     problems: ProblemItem[];
+
+    @v.IsNumber()
     status: number;
+
+    @v.IsString()
     title: string;
+
+    @v.IsString()
     type: string;
 }
