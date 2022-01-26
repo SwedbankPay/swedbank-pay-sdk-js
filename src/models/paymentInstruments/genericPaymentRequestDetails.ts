33aedf8b94602486b8c0b98cfa36efec80e837dc
Xillians <chkxillians@gmail.com>
2022-01-26 13:39:43 +0100

    add intent and operation

--- src/models/paymentInstruments/genericPaymentRequestDetails.ts
+++ src/models/paymentInstruments/genericPaymentRequestDetails.ts
@@ -2,6 +2,8 @@ import * as v from 'class-validator';
 import { BaseModel } from '../generics/baseModel';
 import { Type } from 'class-transformer';
 import { Price } from './price';
+import { PaymentIntent } from './paymentIntent';
+import { Operation } from '../enums/operation';
 
 export class GenericPaymentRequestDetails extends BaseModel {
     @v.IsString()
@@ -10,5 +12,11 @@ export class GenericPaymentRequestDetails extends BaseModel {
     @v.ValidateNested()
     @v.IsArray()
     @Type(() => Price)
-    prices: Price[]
+    prices: Price[];
+
+    @v.IsEnum(PaymentIntent)
+    intent: PaymentIntent;
+
+    @v.IsEnum(Operation)
+    operation: Operation;
 }
