3d7b601de866c1af5e2af32bde8e696109a15343
Xillians <chkxillians@gmail.com>
2022-01-26 10:35:11 +0100

    amount: use class validator

--- src/models/amount.ts
+++ src/models/amount.ts
@@ -1,3 +1,7 @@
-export interface Amount {
+import * as v from 'class-validator';
+import { BaseModel } from './generics/baseModel';
+
+export class Amount extends BaseModel {
+    @v.IsNumber()
     inLowestMonetaryUnit: number;
 }
