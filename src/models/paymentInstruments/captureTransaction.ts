15ce51bdc94adbd7ff77a8f3dfc18a53224df14f
Xillians <chkxillians@gmail.com>
2022-01-26 14:46:05 +0100

    set capture base transaction

--- src/models/paymentInstruments/captureTransaction.ts
+++ src/models/paymentInstruments/captureTransaction.ts
@@ -1,8 +1,27 @@
-import { Amount } from "../generics/amount";
+import * as v from 'class-validator';
+import { Type } from 'class-transformer';
+import { BaseModel } from "../generics/baseModel"
+import { Amount } from '../generics/amount';
 
-export interface CaptureTransaction {
+export class CaptureTransaction extends BaseModel {
+    @Type(() => transactionModel)
+    @v.ValidateNested()
+    transaction: transactionModel
+
+}
+
+class transactionModel extends BaseModel {
+    @v.ValidateNested()
+    @Type(() => Amount)
     amount: Amount;
-    description: string;
-    payeeReference: string;
+    
+    @v.ValidateNested()
+    @Type(() => Amount)
     vatAmount: Amount;
+
+    @v.IsString()
+    description: string;
+
+    @v.IsString()
+    payeeReference: string;
 }
