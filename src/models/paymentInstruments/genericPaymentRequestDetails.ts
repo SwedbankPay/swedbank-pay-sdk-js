963c1603fdf8712e3c90eb643be97700cbd5f5ca
Xillians <chkxillians@gmail.com>
2022-01-26 11:31:18 +0100

    set type for prices

--- src/models/paymentInstruments/genericPaymentRequestDetails.ts
+++ src/models/paymentInstruments/genericPaymentRequestDetails.ts
@@ -1,10 +1,14 @@
 import * as v from 'class-validator';
 import { BaseModel } from '../generics/baseModel';
+import { Type } from 'class-transformer';
 import { Price } from './price';
 
 export class GenericPaymentRequestDetails extends BaseModel {
     @v.IsString()
     description: string;
+
     @v.ValidateNested()
-    prices: Price
+    @v.IsArray()
+    @Type(() => Price)
+    prices: Price[]
 }
