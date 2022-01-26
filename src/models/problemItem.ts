074e3301dfe031e1c811f9fc476899f574c5959b
Xillians <chkxillians@gmail.com>
2022-01-26 11:35:32 +0100

    add validator for problemItem

--- src/models/problemItem.ts
+++ src/models/problemItem.ts
@@ -1,4 +1,10 @@
-export interface ProblemItem {
+import * as v from 'class-validator';
+import { BaseModel } from './generics/baseModel';
+
+export class ProblemItem  extends BaseModel {
+    @v.IsString()
     description: string;
+
+    @v.IsString()
     name: string;
 }
