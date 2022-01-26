5e3fa85f8e93eece2c64bba3be751537611b99b4
Xillians <chkxillians@gmail.com>
2022-01-26 10:35:22 +0100

    price: use class validator

--- src/models/paymentInstruments/price.ts
+++ src/models/paymentInstruments/price.ts
@@ -1,8 +1,16 @@
+import * as v from 'class-validator';
+import { BaseModel } from '../generics/baseModel';
 import { Amount } from "../amount";
 import { PriceType } from "../enums/priceType";
+import { ValidateNested } from 'class-validator';
 
-export interface Price {
+export class Price extends BaseModel {
+    @v.ValidateNested()
     amount: Amount;
-    type: PriceType;
+    
+    @ValidateNested()
     vatAmount: Amount;
+
+    @v.IsEnum(PriceType)
+    type: PriceType;
 }
