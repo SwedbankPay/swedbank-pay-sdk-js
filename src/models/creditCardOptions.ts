b58bada2648e7e6ccc4d4f2fe8b7f0ef7773ead9
Xillians <chkxillians@gmail.com>
2022-01-27 15:06:23 +0100

    creditcard options use baseModel

--- src/models/creditCardOptions.ts
+++ src/models/creditCardOptions.ts
@@ -1,10 +1,29 @@
-export interface CreditCardOptions {
+import * as v from 'class-validator';
+import { BaseModel } from './generics/baseModel';
+
+export class CreditCardOptions extends BaseModel{
+
+    @v.IsBoolean()
     mailOrderTelephoneOrder: boolean;
+    
+    @v.IsBoolean()
     rejectAuthenticationStatusA: boolean;
+    
+    @v.IsBoolean()
     rejectAuthenticationStatusU: boolean;
+    
+    @v.IsBoolean()
     rejectCardNot3DSecureEnrolled: boolean;
+    
+    @v.IsBoolean()
     rejectConsumerCards: boolean;
+    
+    @v.IsBoolean()
     rejectCorporateCards: boolean;
+    
+    @v.IsBoolean()
     rejectCreditCards: boolean;
+    
+    @v.IsBoolean()
     rejectDebitCards: boolean;
 }
