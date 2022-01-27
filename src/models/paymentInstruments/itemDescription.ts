ced58e67426548e150d08184a49dd23f57f3bbe9
Xillians <chkxillians@gmail.com>
2022-01-27 19:45:12 +0100

    validate itemDescription

--- src/models/paymentInstruments/itemDescription.ts
+++ src/models/paymentInstruments/itemDescription.ts
@@ -1,6 +1,13 @@
+import { Type } from 'class-transformer';
+import * as v from 'class-validator';
 import { Amount } from "../generics/amount";
+import { BaseModel } from '../generics/baseModel';
 
-export interface ItemDescription {
+export class ItemDescription extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => Amount)
     amount: Amount;
+
+    @v.IsString()
     description: string;
 }
