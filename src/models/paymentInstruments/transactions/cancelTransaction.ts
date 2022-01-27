366cc3729f255dfbed270a35b7aeef56562cc1c0
Xillians <chkxillians@gmail.com>
2022-01-27 15:17:01 +0100

    moved cancel transaction

--- src/models/paymentInstruments/transactions/cancelTransaction.ts
+++ src/models/paymentInstruments/transactions/cancelTransaction.ts
@@ -1,6 +1,6 @@
 import * as v from 'class-validator';
 import { Type } from 'class-transformer';
-import { BaseModel } from "../generics/baseModel";
+import { BaseModel } from "../../generics/baseModel";
 
 export class CancelTransaction extends BaseModel {
     @Type(() => transactionModel)
