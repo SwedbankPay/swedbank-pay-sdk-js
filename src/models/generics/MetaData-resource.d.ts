976df76811126f2806b4ee9ccfc19450372272c8
Xillians <chkxillians@gmail.com>
2022-01-27 20:12:45 +0100

    remove validator?

--- src/models/generics/MetaData-resource.d.ts
+++ src/models/generics/MetaData-resource.d.ts
@@ -1,7 +1,5 @@
-import * as v from 'class-validator'
 import { GenericResource } from "./GenericResource";
 
 export class MetaData extends GenericResource {
-    @v.IsArray()
     [property: string]: any //eslint-disable  @typescript-eslint/no-explicit-any
 }
