d16c9a0b268777f8bed336b9ed21abc9b62b0afe
Xillians <chkxillians@gmail.com>
2022-01-27 15:18:33 +0100

    moved transaction list response

--- src/models/paymentInstruments/transactions/transactionListResponse.ts
+++ src/models/paymentInstruments/transactions/transactionListResponse.ts
@@ -1,4 +1,4 @@
-import { Identifiable } from "../generics/identifiable";
+import { Identifiable } from "../../generics/identifiable";
 import { Transaction } from "./transaction";
 
 export interface TransactionListResponse extends Identifiable {
