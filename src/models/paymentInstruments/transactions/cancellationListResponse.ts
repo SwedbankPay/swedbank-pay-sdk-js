897fab88ba58e52ef58fadfc66cb2bbabba7cc9a
Xillians <chkxillians@gmail.com>
2022-01-28 10:25:59 +0100

    validate cancellation response

--- src/models/paymentInstruments/transactions/cancellationListResponse.ts
+++ src/models/paymentInstruments/transactions/cancellationListResponse.ts
@@ -1,5 +1,9 @@
+import * as v from 'class-validator';
+import { BaseModel } from '../../generics/baseModel';
 import { Transaction } from "./transaction";
 
-export interface CancellationListResponse {
+export class CancellationListResponse extends BaseModel{
+    // TODO: figure how to validate array content
+    @v.IsArray()
     cancellationList: Transaction[];
 }
