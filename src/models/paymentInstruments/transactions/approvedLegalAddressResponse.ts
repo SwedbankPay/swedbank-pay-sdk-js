07a02f66d485f1f9b168e39e37e73deb9dedb64d
Xillians <chkxillians@gmail.com>
2022-01-28 10:21:47 +0100

    approved legal address uses class validator

--- src/models/paymentInstruments/transactions/approvedLegalAddressResponse.ts
+++ src/models/paymentInstruments/transactions/approvedLegalAddressResponse.ts
@@ -1,6 +1,13 @@
+import * as v from 'class-validator';
+import { Type } from 'class-transformer';
+import { BaseModel } from '../../generics/baseModel';
 import { LegalAddress } from "./legalAddress";
 
-export interface ApprovedLegalAddressResponse {
+export class ApprovedLegalAddressResponse extends BaseModel{
+    @v.IsUrl()
     payment: URL;
+
+    @v.ValidateNested()
+    @Type(() => LegalAddress)
     approvedLegalAddress: LegalAddress;
 }
