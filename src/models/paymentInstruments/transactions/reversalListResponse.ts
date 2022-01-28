788d572793f25a953b20438adc1146cb5384f5e8
Syver <syver.toftemo@payex.com>
2022-01-28 14:50:33 +0100

    correct array validatoin

--- src/models/paymentInstruments/transactions/reversalListResponse.ts
+++ src/models/paymentInstruments/transactions/reversalListResponse.ts
@@ -1,9 +1,11 @@
+import { Type } from 'class-transformer';
 import * as v from 'class-validator';
 import { BaseModel } from '../../generics/baseModel';
 import { TransactionResponse } from "./transactionResponse";
 
 export class ReversalListResponse extends BaseModel{
-    // TODO: add array content validatoin
     @v.IsArray()
+    @v.ValidateNested()
+    @Type(() => TransactionResponse)
     reversalList: TransactionResponse[];
 }
