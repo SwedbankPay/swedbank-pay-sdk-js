c6851e74a9cf1b7efcd10a3b6b9ee9fc243341a5
Xillians <chkxillians@gmail.com>
2022-01-28 10:21:37 +0100

    legal address uses class validator

--- src/models/paymentInstruments/transactions/legalAddress.ts
+++ src/models/paymentInstruments/transactions/legalAddress.ts
@@ -1,9 +1,25 @@
-export interface LegalAddress {
+import { BaseModel } from "../../generics/baseModel";
+import * as v from 'class-validator';
+
+export class LegalAddress extends BaseModel{
+    @v.IsUrl()
     id: URL;
+
+    @v.IsString()
     addressee: string;
+    
+    @v.IsString()
     coAddress: string;
+    
+    @v.IsString()
     streetAddress: string;
+    
+    @v.IsString()
     zipCode: string;
+    
+    @v.IsString()
     city: string;
+    
+    @v.IsString()
     countryCode: string;
 }
