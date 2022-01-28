6fc91d656c786d909c1f893192e58f961c348e22
Syver <syver.toftemo@payex.com>
2022-01-28 12:58:29 +0100

    add validator for capture list response

--- src/models/paymentInstruments/transactions/captureListResponse.ts
+++ src/models/paymentInstruments/transactions/captureListResponse.ts
@@ -1,5 +1,9 @@
+import * as v from 'class-validator';
+import { BaseModel } from '../../generics/baseModel';
 import { Transaction } from "./transaction";
 
-export interface CaptureListResponse {
+export class CaptureListResponse extends BaseModel{
+    // TODO: find out how to validate content of array
+    @v.IsArray()
     captureList: Transaction[];
 }
