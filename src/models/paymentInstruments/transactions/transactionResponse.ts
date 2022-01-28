582e055fa228f472df013e0e1ac7234b4fbc4cbd
Xillians <chkxillians@gmail.com>
2022-01-28 10:31:31 +0100

    validate transaction response base

--- src/models/paymentInstruments/transactions/transactionResponse.ts
+++ src/models/paymentInstruments/transactions/transactionResponse.ts
@@ -1,5 +1,10 @@
+import { Type } from 'class-transformer';
+import * as v from 'class-validator';
+import { BaseModel } from '../../generics/baseModel';
 import { Transaction } from "./transaction";
 
-export interface TransactionResponse {
+export class TransactionResponse extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => Transaction)
     transaction: Transaction;
 }
