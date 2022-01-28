2ccb74d46045dee5f9480418d19fd93290cb5df6
Xillians <chkxillians@gmail.com>
2022-01-28 10:31:42 +0100

    validate cancellation response base

--- src/models/paymentInstruments/transactions/cancellationResponse.ts
+++ src/models/paymentInstruments/transactions/cancellationResponse.ts
@@ -1,6 +1,13 @@
+import * as v from 'class-validator';
+import { Type } from 'class-transformer';
+import { BaseModel } from '../../generics/baseModel';
 import { TransactionResponse } from "./transactionResponse";
 
-export interface CancellationResponse {
+export class CancellationResponse extends BaseModel{
+    @v.IsUrl()
     payment: URL;
+
+    @v.ValidateNested()
+    @Type(() => TransactionResponse)
     cancellation: TransactionResponse;
 }
