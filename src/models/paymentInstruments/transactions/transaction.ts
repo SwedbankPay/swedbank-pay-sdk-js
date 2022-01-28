8f8c448845acdef53d538ab741c516945038d541
Xillians <chkxillians@gmail.com>
2022-01-28 10:31:23 +0100

    validate transaction base

--- src/models/paymentInstruments/transactions/transaction.ts
+++ src/models/paymentInstruments/transactions/transaction.ts
@@ -1,22 +1,53 @@
+import * as v from 'class-validator';
 import { Amount } from "../../generics/amount";
 import { HttpOperation } from "../../generics/httpOperation";
 import { Identifiable } from "../../generics/identifiable";
 import { Problem } from "../../generics/problem";
 import { State } from "../../enums/state";
 import { TransactionType } from "../../enums/transactionType";
+import { Type } from 'class-transformer';
 
-export interface Transaction extends Identifiable {
+export class Transaction extends Identifiable {
+    @v.IsString()
     created: string;
+    
+    @v.IsString()
     updated: string;
+    
+    @v.IsString()
     activity: string;
+    
+    @Type(() => Amount)
+    @v.ValidateNested()
     amount: Amount;
+    
+    @v.IsString()
     description: string;
+    
+    @v.IsBoolean()
     isOperational: boolean;
+    
+    @v.IsNumber()
     number: number;
+    
+    // TODO: Figure out how to validate content of Array
+    @v.IsArray()
     operations: HttpOperation[];
+    
+    @v.IsString()
     payeeReference: string;
+    
+    @v.IsEnum(State)
     state: State;
+    
+    @v.IsEnum(TransactionType)
     type: TransactionType;
+    
+    @v.ValidateNested()
+    @Type(() => Amount)
     vatAmount: Amount;
+    
+    @v.ValidateNested()
+    @Type(() => Problem)
     problem: Problem;
 }
