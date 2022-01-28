00c805179b04e4a69fd4aa6471356c278bc3279c
Syver <syver.toftemo@payex.com>
2022-01-28 14:10:08 +0100

    payment abort request details validated

--- src/models/paymentInstruments/transactions/paymentAbortRequestDetails.ts
+++ src/models/paymentInstruments/transactions/paymentAbortRequestDetails.ts
@@ -1,4 +1,10 @@
-export interface PaymentAbortRequestDetails {
+import * as v from 'class-validator';
+import { BaseModel } from '../../generics/baseModel';
+
+export class PaymentAbortRequestDetails extends BaseModel{
+    @v.IsString()
     abortReason: string;
+
+    @v.IsString()
     operation: string;
 }
