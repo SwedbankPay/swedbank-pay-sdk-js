d16ac2d161299f2c52b57b60411b112dd7b6f576
Xillians <chkxillians@gmail.com>
2022-01-31 13:30:09 +0100

    validation on shipping details

--- src/models/consumers/shippingDetails.ts
+++ src/models/consumers/shippingDetails.ts
@@ -1,9 +1,20 @@
-import { Address } from "cluster";
+import { Type } from 'class-transformer';
+import * as v from 'class-validator';
+import { Address } from '../address';
 import { EmailAddress } from "../emailAddress";
+import { BaseModel } from "../generics/baseModel";
 import { Msisdn } from "../msisdn";
 
-export interface ShippingDetails {
+export class ShippingDetails extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => EmailAddress)
     email: EmailAddress;
+
+    @v.ValidateNested()
+    @Type(() => Msisdn)
     msisdn: Msisdn;
+
+    @v.ValidateNested()
+    @Type(() => Address)
     shippingAddress: Address;
 }
