414806b3ee3bea81378f1230d4987bd37aac1228
Xillians <chkxillians@gmail.com>
2022-01-31 13:26:47 +0100

    address validated

--- src/models/consumers/address.ts
+++ src/models/consumers/address.ts
@@ -1,8 +1,24 @@
-export interface Address {
+import * as v from 'class-validator';
+import { BaseModel } from "../generics/baseModel";
+
+export class Address extends BaseModel {
+    @v.IsString()
     addressee: string;
+    
+    @v.IsString()
     city: string;
+    
+    @v.IsString()
     coAddress: string;
+    
+    @v.IsString()
+    @v.IsUppercase()
+    @v.Length(2, 2)
     countryCode: string;
+    
+    @v.IsString()
     streetAddress: string;
+    
+    @v.IsString()
     zipCode: string;
 }
