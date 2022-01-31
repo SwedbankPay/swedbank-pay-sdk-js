6cbbfd460d8f6c52fb34fd5ed12f2ca85397f339
Xillians <chkxillians@gmail.com>
2022-01-31 13:28:02 +0100

    consumer operations validated

--- src/models/consumers/consumerOperations.ts
+++ src/models/consumers/consumerOperations.ts
@@ -1,7 +1,13 @@
+import { Type } from 'class-transformer';
+import * as v from 'class-validator';
 import { HttpOperation } from "../generics/httpOperation";
 import OperationsBase from "../generics/operationsBase";
 
-export interface ConsumerOperations extends OperationsBase {
+export class ConsumerOperations extends OperationsBase {
+    @v.ValidateNested()
+    @Type(() => HttpOperation)
     redirectConsumerIdentification: HttpOperation;
+    @v.ValidateNested()
+    @Type(() => HttpOperation)
     viewConsumerIdentification: HttpOperation;
 }
