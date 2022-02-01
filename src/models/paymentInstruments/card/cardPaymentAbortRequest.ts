fb9a7aa96ef426ad8b3cfe95050b5194539551e3
Xillians <chkxillians@gmail.com>
2022-02-01 09:25:50 +0100

    validate abort card payment request

--- src/models/paymentInstruments/card/cardPaymentAbortRequest.ts
+++ src/models/paymentInstruments/card/cardPaymentAbortRequest.ts
@@ -1,5 +1,10 @@
+import { BaseModel } from "../../generics/baseModel";
 import { CardPaymentAbortPayment } from "./cardPaymentAbortPayment";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface CardPaymentAbortRequest {
+export class CardPaymentAbortRequest extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => CardPaymentAbortPayment)
     payment: CardPaymentAbortPayment;
 }
