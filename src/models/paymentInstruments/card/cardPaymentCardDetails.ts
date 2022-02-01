10fe65981ee4244998799e7dfdfe8f731c7f1ac1
Xillians <chkxillians@gmail.com>
2022-02-01 09:20:27 +0100

    validate card payment details

--- src/models/paymentInstruments/card/cardPaymentCardDetails.ts
+++ src/models/paymentInstruments/card/cardPaymentCardDetails.ts
@@ -1,7 +1,19 @@
-export interface CardPaymentCardDetails {
+import * as v from 'class-validator';
+import { BaseModel } from "../../generics/baseModel";
+
+export class CardPaymentCardDetails extends BaseModel{
+    @v.IsNumber()
     cardExpiryMonth: number;
+
+    @v.IsNumber()
     cardExpiryYear: number;
+
+    @v.IsString()
     cardHolderName: string;
+
+    @v.IsString()
     cardNumber: string;
+
+    @v.IsString()
     cardVerificationCode: string;
 }
