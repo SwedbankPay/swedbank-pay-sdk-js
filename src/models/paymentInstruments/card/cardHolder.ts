be41e7c476f575b79afe953322612e78abd6005b
Xillians <chkxillians@gmail.com>
2022-02-01 09:09:40 +0100

    validate card holder

--- src/models/paymentInstruments/card/cardHolder.ts
+++ src/models/paymentInstruments/card/cardHolder.ts
@@ -1,16 +1,44 @@
-import { Address } from "cluster";
+import { Type } from "class-transformer";
+import * as v from "class-validator";
 import { AccountInfo } from "../../accountInfo";
+import { Address } from "../../address";
 import { EmailAddress } from "../../emailAddress";
+import { BaseModel } from "../../generics/baseModel";
 import { Msisdn } from "../../msisdn";
 
-export interface Cardholder {
+export class Cardholder extends BaseModel{
+
+    @v.ValidateNested()
+    @Type(() => AccountInfo)
     accountInfo: AccountInfo;
+    
+    @v.ValidateNested()
+    @Type(() => Address)
     billingAddress: Address;
+    
+    @v.ValidateNested()
+    @Type(() => EmailAddress)
     email: EmailAddress;
+    
+    @v.IsString()
     firstName: string;
+    
+    @v.IsString()
     lastName: string;
+    
+    @v.ValidateNested()
+    @Type(() => Msisdn)
     homePhoneNumber: Msisdn;
+    
+    @v.ValidateNested()
+    @Type(() => Msisdn)
     msisdn: Msisdn;
+    
+    @v.ValidateNested()
+    @Type(() => Address)
     shippingaddress: Address;
+    
+    @v.ValidateNested()
+    @Type(() => Msisdn)
     workPhoneNumber: Msisdn;
 }
