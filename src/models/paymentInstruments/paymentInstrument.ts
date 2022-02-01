936205d1ae28d2fea47baa02b6bbfae8ac099599
Xillians <chkxillians@gmail.com>
2022-02-01 09:17:45 +0100

    validate payment instrument

--- src/models/paymentInstruments/paymentInstrument.ts
+++ src/models/paymentInstruments/paymentInstrument.ts
@@ -1,4 +1,4 @@
-import { PayeeInfo } from "../../Services/PaymentOrder/models/PayeeInfo";
+import { PayeeInfo } from '../generics/payeeInfo'
 import { Amount } from "../generics/amount";
 import { Currency } from "../currency";
 import { Identifiable } from "../generics/identifiable";
@@ -13,32 +13,97 @@ import { PaymentIntent } from "./enums/paymentIntent";
 import { PriceListResponse } from "./priceListResponse";
 import { ReversalListResponse } from "./transactions/reversalListResponse";
 import { TransactionListResponse } from "./transactions/transactionListResponse";
+import * as v from 'class-validator';
+import { Type } from 'class-transformer';
 
-export interface PaymentInstrument extends Identifiable {
+export class PaymentInstrument extends Identifiable {
+    
+    @v.ValidateNested()
+    @Type(() => Amount)
     amount: Amount;
+    
+    @v.ValidateNested()
+    @Type(() => CancellationListResponse)
     cancellations: CancellationListResponse;
+
+    @v.ValidateNested()
+    @Type(() => CaptureListResponse)
     captures: CaptureListResponse;
+
+    @v.IsString()
     created: string;
+
+    @v.IsString()
     updated: string;
+
+    @v.ValidateNested()
+    @Type(() => Currency)
     currency: Currency;
+
+    @v.IsString()
     description: string;
-    instrument: PaymentInstrument;
+
+    @v.IsEnum(PaymentIntent)
     intent: PaymentIntent;
+
+    @v.ValidateNested()
+    @Type(() => Language)
     language: Language;
+
+    @v.IsNumber()
     number: number;
+
+    @v.IsEnum(Operation)
     operation: Operation;
+
+    @v.ValidateNested()
+    @Type(() => PayeeInfo)
     payeeInfo: PayeeInfo;
+
+    @v.IsString()
     payerReference: string;
+
+    @v.IsString()
     initiatingSystemUserAgent: string;
+
+    @v.ValidateNested()
+    @Type(() => PriceListResponse)
     prices: PriceListResponse;
+
+    @v.ValidateNested()
+    @Type(() => Amount)
     remainingCancellationAmount: Amount;
+
+    @v.ValidateNested()
+    @Type(() => Amount)
     remainingCaptureAmount: Amount;
+
+    @v.ValidateNested()
+    @Type(() => Amount)
     remainingReversalAmount: Amount;
+
+    @v.IsEnum(State)
     state: State;
+
+    @v.ValidateNested()
+    @Type(() => TransactionListResponse)
     transactions: TransactionListResponse;
+
+    @v.IsUrl()
     urls: Urls;
+
+    @v.IsString()
     userAgent: string;
+
+    @v.ValidateNested()
+    @Type(() => Amount)
     vatAmount: Amount;
+
+    @v.ValidateNested()
+    @Type(() => ReversalListResponse)
     reversals: ReversalListResponse;
+
+    @v.ValidateNested()
+    @Type(() => MetaData)
     metadata: MetaData;
 }
