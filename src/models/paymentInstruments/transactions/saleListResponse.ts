2a363225dc6ea9d5c320c1b86e5ce603d4913bb4
Xillians <chkxillians@gmail.com>
2022-02-14 11:02:03 +0100

    add sale rlist response

---
+++ src/models/paymentInstruments/transactions/saleListResponse.ts
@@ -0,0 +1,11 @@
+import { Type } from 'class-transformer';
+import * as v from 'class-validator';
+import { BaseModel } from '../../generics/baseModel';
+import { TransactionResponse } from "./transactionResponse";
+
+export class SaleListResponse extends BaseModel{
+    @v.IsArray()
+    @v.ValidateNested()
+    @Type(() => TransactionResponse)
+    saleList: TransactionResponse[];
+}
