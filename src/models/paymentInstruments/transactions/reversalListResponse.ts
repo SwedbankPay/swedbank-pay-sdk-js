54a183fc1f8d0667c65d5a6907321380f4bc8b77
Xillians <chkxillians@gmail.com>
2022-02-14 11:22:38 +0100

    fix what transaction type is used

--- src/models/paymentInstruments/transactions/reversalListResponse.ts
+++ src/models/paymentInstruments/transactions/reversalListResponse.ts
@@ -1,11 +1,11 @@
 import { Type } from 'class-transformer';
 import * as v from 'class-validator';
 import { BaseModel } from '../../generics/baseModel';
-import { TransactionResponse } from "./transactionResponse";
+import { ReversalResponse } from "./reversalResponse";
 
 export class ReversalListResponse extends BaseModel{
     @v.IsArray()
     @v.ValidateNested()
-    @Type(() => TransactionResponse)
-    reversalList: TransactionResponse[];
+    @Type(() => ReversalResponse)
+    reversalList: ReversalResponse[];
 }
