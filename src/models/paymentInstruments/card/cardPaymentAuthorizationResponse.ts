5b82a3ada520c8550afaac1a69b846e8ca5bce0e
Xillians <chkxillians@gmail.com>
2022-02-14 11:03:12 +0100

    validate card authorization response

--- src/models/paymentInstruments/card/cardPaymentAuthorizationResponse.ts
+++ src/models/paymentInstruments/card/cardPaymentAuthorizationResponse.ts
@@ -1,6 +1,13 @@
+import * as v from 'class-validator';
+import { Type } from 'class-transformer';
+import { BaseModel } from "../../generics/baseModel";
 import { CardPaymentAuthorization } from "./cardPaymentAuthorization";
 
-export interface CardPaymentAuthorizationResponse {
+export class CardPaymentAuthorizationResponse extends BaseModel {
+    @v.ValidateNested()
+    @Type(() => CardPaymentAuthorization)
     authorization: CardPaymentAuthorization;
+
+    @v.IsUrl()
     payment: URL;
 }
