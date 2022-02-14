47138831ae80952e3d6f98cab843d8b0fc25b633
Xillians <chkxillians@gmail.com>
2022-02-14 11:01:53 +0100

    add generic sale response

---
+++ src/models/paymentInstruments/transactions/saleResponse.ts
@@ -0,0 +1,13 @@
+import { Type } from 'class-transformer';
+import * as v from 'class-validator';
+import { BaseModel } from '../../generics/baseModel';
+import { TransactionResponse } from "./transactionResponse";
+
+export class SaleResponse extends BaseModel {
+    @v.IsUrl()
+    payment: URL;
+
+    @v.ValidateNested()
+    @Type(() => TransactionResponse)
+    sale: TransactionResponse;
+}
