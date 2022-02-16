ee656bec2e5b0c4d3b22898b8a3380e7c8074825
Xillians <chkxillians@gmail.com>
2022-02-16 12:07:54 +0100

    validate card recur using generic get

--- src/models/paymentInstruments/card/cardPaymentRecurResponseDetails.ts
+++ src/models/paymentInstruments/card/cardPaymentRecurResponseDetails.ts
@@ -1,35 +1,9 @@
-import { PayeeInfo } from "../../../Services/PaymentOrder/models/PayeeInfo";
 import { Amount } from "../../generics/amount";
-import { Currency } from "../../currency";
-import { Identifiable } from "../../generics/identifiable";
-import { MetaData } from "../../generics/MetaData-resource";
-import { Operation } from "../../enums/operation";
-import { State } from "../../enums/state";
-import { Urls } from "../../generics/urls";
-import { PaymentInstrument } from "../paymentInstrument";
-import { PriceListResponse } from "../priceListResponse";
-import { TransactionListResponse } from "../transactions/transactionListResponse";
-import { CardPaymentAuthorization } from "./cardPaymentAuthorization";
+import { GetPaymentResponse } from "../transactions/getPaymentResponse";
 
-export interface CardPaymentRecurResponseDetails extends Identifiable {
+export class CardPaymentRecurResponseDetails extends GetPaymentResponse {
+    paymentToken: string;
     recurrenceToken: string;
-    number: number;
-    created: string;
-    updated: string;
-    instrument: PaymentInstrument;
-    operation: Operation;
-    state: State;
-    currency: Currency;
-    prices: PriceListResponse;
-    amount: Amount;
     remainingCaptureAmount: Amount;
     remainingCancellationAmount: Amount;
-    description: string;
-    initiatingSystemUserAgent: string;
-    userAgent: string;
-    transactions: TransactionListResponse;
-    authorizations: CardPaymentAuthorization;
-    urls: Urls;
-    payeeInfo: PayeeInfo;
-    metaData: MetaData;
 }
