fa0c66439e51c3f529c7a8316d339acbef40debe
Xillians <chkxillians@gmail.com>
2022-03-07 09:42:33 +0100

    Add validation card payment reversal transaction

--- src/models/paymentInstruments/card/cardPaymentReversalTransaction.ts
+++ src/models/paymentInstruments/card/cardPaymentReversalTransaction.ts
@@ -1,8 +1,20 @@
 import { Amount } from "../../generics/amount";
+import { BaseModel } from "../../generics/baseModel";
+import * as v from 'class-validator';
+import { Type } from 'class-transformer';
 
-export interface CardPaymentReversalTransaction {
+export class CardPaymentReversalTransaction extends BaseModel {
+    @v.ValidateNested()
+    @Type(() => Amount)
     amount: Amount;
+
+    @v.IsString()
     description: string;
+
+    @v.IsString()
     payeeReference: string;
+
+    @v.ValidateNested()
+    @Type(() => Amount)
     vatAmount: Amount;
 }
