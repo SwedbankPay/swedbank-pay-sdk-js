63377de0c2a86f26212e4868f62b25552f4f3da3
Xillians <chkxillians@gmail.com>
2022-03-23 13:30:13 +0100

    validate verify request

--- src/models/paymentInstruments/card/cardPaymentVerifyRequest.ts
+++ src/models/paymentInstruments/card/cardPaymentVerifyRequest.ts
@@ -1,5 +1,9 @@
 import { CardPaymentVerifyRequestDetails } from "./cardPaymentVerifyRequestDetails";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface CardPaymentVerifyRequest {
+export class CardPaymentVerifyRequest {
+    @v.ValidateNested()
+    @Type(() => CardPaymentVerifyRequestDetails)
     payment: CardPaymentVerifyRequestDetails;
 }
