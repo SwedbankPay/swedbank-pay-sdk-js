1c84e789df17e7cf541aceb6a001c2c21c11f59a
Xillians <chkxillians@gmail.com>
2022-03-23 13:40:52 +0100

    add verify response details validation

--- src/models/paymentInstruments/card/cardPaymentVerifyResponseDetails.ts
+++ src/models/paymentInstruments/card/cardPaymentVerifyResponseDetails.ts
@@ -1,6 +1,11 @@
 import { Identifiable } from "../../generics/identifiable";
 import { CardPaymentVerification } from "./cardPaymentVerification";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface CardPaymentVerifyResponseDetails extends Identifiable {
+export class CardPaymentVerifyResponseDetails extends Identifiable {
+    @v.IsArray()
+    @v.ValidateNested()
+    @Type(() => CardPaymentVerification)
     verificationList: CardPaymentVerification[];
 }
