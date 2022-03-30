a694de50bec9da895eadd1294725142e426ed680
Xillians <chkxillians@gmail.com>
2022-03-30 12:38:05 +0200

    capture transaction validated

--- src/models/paymentInstruments/invoice/invoicePaymentCaptureRequest.ts
+++ src/models/paymentInstruments/invoice/invoicePaymentCaptureRequest.ts
@@ -1,5 +1,34 @@
+import { BaseModel } from "../../generics/baseModel";
 import { CaptureTransaction } from "../transactions/captureTransaction";
+import * as v from 'class-validator'
+import { Type } from "class-transformer";
 
-export interface InvoicePaymentCaptureRequest {
-    transaction: CaptureTransaction;
+export class InvoicePaymentCaptureReqest extends CaptureTransaction{
+    @v.IsArray()
+    @Type(() => ItemDescription)
+    itemDescriptions: ItemDescription[];
+
+    @v.IsArray()
+    @Type(() => vatSummary)
+    vatSummary: vatSummary[];
+}
+
+class ItemDescription extends BaseModel {
+    @v.IsNumber()
+    amount: number;
+
+    @v.IsString()
+    description: string;
+}
+
+class vatSummary extends BaseModel {
+    
+    @v.IsNumber()
+    amount: number;
+    
+    @v.IsNumber()
+    vatPercent: number;
+    
+    @v.IsNumber()
+    vatAmount: number;
 }
