49df5393ea8e3856d32697d255be81710da2bacc
Xillians <chkxillians@gmail.com>
2022-03-30 12:37:22 +0200

    invoice capture validated

--- src/models/paymentInstruments/invoice/invoicePaymentReversalRequest.ts
+++ src/models/paymentInstruments/invoice/invoicePaymentReversalRequest.ts
@@ -1,5 +1,10 @@
-import { ReversalTransaction } from "./reversalTransaction";
+import { ReversalRequestDetails } from "./reversalTransaction";
+import * as v from 'class-validator'
+import { Type } from "class-transformer";
+import { BaseModel } from "../../generics/baseModel";
 
-export interface InvoicePaymentReversalRequest {
-    transaction: ReversalTransaction;
+export class InvoicePaymentReversalRequest extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => ReversalRequestDetails)
+    transaction: ReversalRequestDetails;
 }
