703eb37ec701caa64c1c54a352de9b551856f575
Xillians <chkxillians@gmail.com>
2022-03-30 12:38:26 +0200

    approved legal address validated

--- src/models/paymentInstruments/invoice/approvedLegalAddressRequest.ts
+++ src/models/paymentInstruments/invoice/approvedLegalAddressRequest.ts
@@ -1,4 +1,17 @@
-export interface ApprovedLegalAddressRequest {
+import * as v from 'class-validator';
+import { Type } from 'class-transformer';
+import { BaseModel } from '../../generics/baseModel';
+
+export class ApprovedLegalAddressRequest extends BaseModel {
+    @v.ValidateNested()
+    @Type(() => LegalAddress)
+    addressee: LegalAddress;
+}
+
+class LegalAddress extends BaseModel{
+    @v.IsString()
     socialSecurityNumber: string;
+
+    @v.IsString()
     zipCode: string;
 }
