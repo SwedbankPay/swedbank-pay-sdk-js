0d4cffeb01b4c3b4a66ed2e2b30afecf7a56f213
Xillians <chkxillians@gmail.com>
2022-04-04 13:02:43 +0200

    sale list item added validation

--- src/models/paymentInstruments/swish/swishSaleListItem.ts
+++ src/models/paymentInstruments/swish/swishSaleListItem.ts
@@ -1,11 +1,25 @@
+import { Type } from 'class-transformer';
+import * as  v from 'class-validator';
 import { Identifiable } from "../../generics/identifiable";
 import { Transaction } from "../transactions/transaction";
 
-export interface SwishSaleListItem extends Identifiable {
+export class SwishSaleListItem extends Identifiable {
+    @v.IsString()
     date: string;
+    
+    @v.IsString()
     payerAlias: string;
+    
+    @v.IsString()
     paymentRequestToken: string;
+    
+    @v.IsString()
     swishPaymentReference: string;
+    
+    @v.IsString()
     swishStatus: string;
+
+    @v.ValidateNested()
+    @Type(() => Transaction)
     transaction: Transaction;
 }
