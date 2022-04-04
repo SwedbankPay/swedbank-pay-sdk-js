0d4dd962919b14285f342c50acf06cc5a6b4a6ca
Xillians <chkxillians@gmail.com>
2022-04-04 14:16:27 +0200

    validating prefill info

--- src/models/paymentInstruments/trustly/trustlyPrefillInfo.ts
+++ src/models/paymentInstruments/trustly/trustlyPrefillInfo.ts
@@ -1,4 +1,10 @@
-export interface TrustlyPrefillInfo {
+import * as v from 'class-validator';
+import { BaseModel } from '../../generics/baseModel';
+
+export class TrustlyPrefillInfo extends BaseModel {
+    @v.IsString()
     firstName: string;
+    
+    @v.IsString()
     lastName: string;
 }
