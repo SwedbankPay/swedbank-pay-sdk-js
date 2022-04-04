22bab5a4d5298c8871a2e00fed69db7f15e64900
Xillians <chkxillians@gmail.com>
2022-04-04 10:23:58 +0200

    Add id to transaction response

--- src/models/paymentInstruments/transactions/transactionResponse.ts
+++ src/models/paymentInstruments/transactions/transactionResponse.ts
@@ -1,9 +1,9 @@
 import { Type } from 'class-transformer';
 import * as v from 'class-validator';
-import { BaseModel } from '../../generics/baseModel';
+import { Identifiable } from '../../generics/identifiable';
 import { Transaction } from "./transaction";
 
-export class TransactionResponse extends BaseModel{
+export class TransactionResponse extends Identifiable{
     @v.ValidateNested()
     @Type(() => Transaction)
     transaction: Transaction;
