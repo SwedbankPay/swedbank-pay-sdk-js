2b46d6ef8e6d9e7160ef56dab64a01ce6ff1b103
Xillians <chkxillians@gmail.com>
2022-04-04 13:02:34 +0200

    sale list added validation

--- src/models/paymentInstruments/swish/swishSaleListResponse.ts
+++ src/models/paymentInstruments/swish/swishSaleListResponse.ts
@@ -1,6 +1,11 @@
+import { Type } from 'class-transformer';
+import * as v from 'class-validator';
 import { Identifiable } from "../../generics/identifiable";
 import { SwishSaleListItem } from "./swishSaleListItem";
 
-export interface SwishSaleListResponse extends Identifiable {
+export class SwishSaleListResponse extends Identifiable {
+    @v.IsArray()
+    @v.ValidateNested()
+    @Type(() => SwishSaleListItem)
     saleList: SwishSaleListItem[];
 }
