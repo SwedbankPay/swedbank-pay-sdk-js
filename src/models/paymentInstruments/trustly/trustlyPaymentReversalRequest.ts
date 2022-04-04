5bdaef0ac188fcade449ddd37031b4ba4a7b1735
Xillians <chkxillians@gmail.com>
2022-04-04 14:17:05 +0200

    reversal request using validation

--- src/models/paymentInstruments/trustly/trustlyPaymentReversalRequest.ts
+++ src/models/paymentInstruments/trustly/trustlyPaymentReversalRequest.ts
@@ -1,5 +1,10 @@
+import { BaseModel } from "../../generics/baseModel";
 import { TrustlyReversalTransaction } from "./trustlyReversalTransaction";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface TrustlyPaymentReversalRequest {
+export class TrustlyPaymentReversalRequest extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => TrustlyReversalTransaction)
     transaction: TrustlyReversalTransaction;
 }
