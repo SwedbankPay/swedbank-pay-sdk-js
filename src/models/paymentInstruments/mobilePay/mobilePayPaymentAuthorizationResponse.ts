669130c5529e399855e6bfeae33ce5d508635565
Xillians <chkxillians@gmail.com>
2022-04-04 10:26:17 +0200

    authorization response added

--- src/models/paymentInstruments/mobilePay/mobilePayPaymentAuthorizationResponse.ts
+++ src/models/paymentInstruments/mobilePay/mobilePayPaymentAuthorizationResponse.ts
@@ -1,6 +1,10 @@
+import * as v from 'class-validator';
+import { Type } from 'class-transformer';
+import { TransactionResponse } from "../transactions/transactionResponse";
 import { MobilePayPaymentAuthorization } from "./mobilePayPaymentAuthorization";
 
-export interface MobilePayPaymentAuthorizationResponse {
+export class MobilePayPaymentAuthorizationResponse extends TransactionResponse{
+    @v.ValidateNested()
+    @Type(() => MobilePayPaymentAuthorization)
     authorization: MobilePayPaymentAuthorization;
-    payment: URL;
 }
