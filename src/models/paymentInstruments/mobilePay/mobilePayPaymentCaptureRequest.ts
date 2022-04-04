39545090dda114896678f20fe4fbd5dbccd9ec1b
Xillians <chkxillians@gmail.com>
2022-04-04 10:25:30 +0200

    Capture request validated

--- src/models/paymentInstruments/mobilePay/mobilePayPaymentCaptureRequest.ts
+++ src/models/paymentInstruments/mobilePay/mobilePayPaymentCaptureRequest.ts
@@ -1,5 +1,10 @@
 import { CaptureTransaction } from "../transactions/captureTransaction";
+import { BaseModel } from "../../generics/baseModel";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface MobilePayPaymentCaptureRequest {
+export class MobilePayPaymentCaptureRequest extends BaseModel {
+    @v.ValidateNested()
+    @Type(() => CaptureTransaction)
     transaction: CaptureTransaction;
 }
