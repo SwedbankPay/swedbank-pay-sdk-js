4f4ce28864487ae0f8ce2461928d8a4481af11f7
Xillians <chkxillians@gmail.com>
2022-04-04 14:17:19 +0200

    payment request validated

--- src/models/paymentInstruments/trustly/trustlyPaymentRequest.ts
+++ src/models/paymentInstruments/trustly/trustlyPaymentRequest.ts
@@ -1,5 +1,10 @@
 import { TrustlyPaymentDetails } from "./trustlyPaymentDetails";
+import { BaseModel } from "../../generics/baseModel";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface TrustlyPaymentRequest {
+export class TrustlyPaymentRequest extends BaseModel {
+    @v.ValidateNested()
+    @Type(() => TrustlyPaymentDetails)
     payment: TrustlyPaymentDetails;
 }
