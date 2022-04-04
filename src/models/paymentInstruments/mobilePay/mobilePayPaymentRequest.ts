a1a193209f78afc4688729c2e5cb6d59ea90537b
Xillians <chkxillians@gmail.com>
2022-04-04 10:27:25 +0200

    mobilepay payment request validation

--- src/models/paymentInstruments/mobilePay/mobilePayPaymentRequest.ts
+++ src/models/paymentInstruments/mobilePay/mobilePayPaymentRequest.ts
@@ -1,7 +1,15 @@
+import { BaseModel } from "../../generics/baseModel";
 import { MobilePayPaymentDetails } from "./mobilePayPaymentDetails";
 import { MobilePayRequestData } from "./mobilePayRequestData";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface MobilePayPaymentRequest {
+export class MobilePayPaymentRequest extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => MobilePayPaymentDetails)
     payment: MobilePayPaymentDetails;
+
+    @v.ValidateNested()
+    @Type(() => MobilePayRequestData)
     mobilePay: MobilePayRequestData;
 }
