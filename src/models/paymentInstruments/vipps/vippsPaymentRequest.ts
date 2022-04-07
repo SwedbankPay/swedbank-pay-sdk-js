566de9e6ce3cf59838ff1394e318bdd4686ce4e2
Xillians <chkxillians@gmail.com>
2022-04-07 12:16:22 +0200

    validate vipps payment request

--- src/models/paymentInstruments/vipps/vippsPaymentRequest.ts
+++ src/models/paymentInstruments/vipps/vippsPaymentRequest.ts
@@ -1,5 +1,10 @@
 import { VippsPaymentRequestDetails } from "./vippsPaymentRequestDetails";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
+import { BaseModel } from "../../generics/baseModel";
 
-export interface VippsPaymentRequest {
+export class VippsPaymentRequest extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => VippsPaymentRequestDetails)
     payment: VippsPaymentRequestDetails;
 }
