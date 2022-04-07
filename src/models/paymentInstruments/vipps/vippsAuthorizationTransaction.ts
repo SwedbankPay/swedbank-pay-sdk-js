9db235f3b0cbb5fc998286b032406882de4cbaf2
Xillians <chkxillians@gmail.com>
2022-04-07 12:13:33 +0200

    validated authorization transaction

--- src/models/paymentInstruments/vipps/vippsAuthorizationTransaction.ts
+++ src/models/paymentInstruments/vipps/vippsAuthorizationTransaction.ts
@@ -1,3 +1,7 @@
-export interface VippsAuthorizationTransaction {
+import * as v from 'class-validator';
+import { BaseModel } from "../../generics/baseModel";
+
+export class VippsAuthorizationTransaction extends BaseModel {
+    @v.IsString()
     msisdn: string;
 }
