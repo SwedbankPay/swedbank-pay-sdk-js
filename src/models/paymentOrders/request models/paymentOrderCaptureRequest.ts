1af37e9c216806d8aa684ef6449cd7e69fddfee6
Xillians <chkxillians@gmail.com>
2022-04-07 12:45:32 +0200

    validate capture request

--- src/models/paymentOrders/request models/paymentOrderCaptureRequest.ts
+++ src/models/paymentOrders/request models/paymentOrderCaptureRequest.ts
@@ -1,5 +1,10 @@
 import { PaymentOrderCaptureTransaction } from "./paymentOrderCaptureTransaction";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
+import { BaseModel } from "../../generics/baseModel";
 
-export interface PaymentOrderCaptureRequest {
+export class PaymentOrderCaptureRequest extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => PaymentOrderCaptureTransaction)
     transaction: PaymentOrderCaptureTransaction;
 }
