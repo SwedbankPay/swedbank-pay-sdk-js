8c9f1bac9b5c7939ca2348e7f8ea06437df72782
Xillians <chkxillians@gmail.com>
2022-04-07 12:14:19 +0200

    validate authorization

--- src/models/paymentInstruments/vipps/vippsPaymentAuthorization.ts
+++ src/models/paymentInstruments/vipps/vippsPaymentAuthorization.ts
@@ -1,7 +1,37 @@
+import { Identifiable } from "../../generics/identifiable";
 import { AuthorizationTransaction } from "../transactions/authorizationTransaction";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface VippsPaymentAuthorization {
-    mobileNumber: string;
-    transaction: AuthorizationTransaction;
-    vippsTransactionId: string;
+export class VippsPaymentAuthorization extends Identifiable{
+    @v.IsString()
+    direct: boolean;
+
+    @v.IsString()
+    cardBrand: string;
+
+    @v.IsString()
+    cardType: string;
+
+    @v.IsString()
+    issuingBank: string;
+
+    @v.IsString()
+    maskedPan: string;
+
+    @v.IsString()
+    expiryDate: string;
+
+    @v.IsString()
+    panToken: string;
+
+    @v.IsBoolean()
+    panEnrolled: boolean;
+
+    @v.IsString()
+    acquirerTransactionTime: string;
+
+    @v.ValidateNested()
+    @Type(() => AuthorizationTransaction)
+    transaction: AuthorizationTransaction
 }
