79ddbfca298775cf2e8408b0aa7204becf6cc927
Xillians <chkxillians@gmail.com>
2022-04-07 12:34:38 +0200

    validate paymentorder cancel

--- src/models/paymentOrders/request models/paymentOrderCancelRequest.ts
+++ src/models/paymentOrders/request models/paymentOrderCancelRequest.ts
@@ -1,5 +1,10 @@
+import { BaseModel } from "../../generics/baseModel";
 import { PaymentOrderCancelTransaction } from "./paymentOrderCancelTransaction";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface PaymentOrderCancelRequest {
+export class PaymentOrderCancelRequest extends BaseModel {
+    @v.ValidateNested()
+    @Type(() => PaymentOrderCancelTransaction)
     transaction: PaymentOrderCancelTransaction;
 }
--- src/models/paymentOrders/request models/paymentOrderCancelTransaction.ts
+++ src/models/paymentOrders/request models/paymentOrderCancelTransaction.ts
@@ -1,4 +1,10 @@
-export interface PaymentOrderCancelTransaction {
+import { BaseModel } from "../../generics/baseModel";
+import * as v from 'class-validator'
+
+export class PaymentOrderCancelTransaction extends BaseModel{
+    @v.IsString()
     description: string;
+
+    @v.IsString()
     payeeReference: string;
 }
