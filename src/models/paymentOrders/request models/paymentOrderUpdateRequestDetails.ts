e062fa32dbc7803be8960689c5a8b68b5573dcc2
Xillians <chkxillians@gmail.com>
2022-04-07 12:27:08 +0200

    moved item to request folder

---
+++ src/models/paymentOrders/request models/paymentOrderUpdateRequestDetails.ts
@@ -0,0 +1,10 @@
+import { OrderItem } from "../../../Services/PaymentOrder/models/OrderItem";
+import { Amount } from "../../generics/amount";
+import { Operation } from "../../enums/operation";
+
+export interface PaymentOrderUpdateRequestDetails {
+    amount: Amount;
+    operation: Operation;
+    vatAmount: Amount;
+    orderItems: OrderItem[];
+}
