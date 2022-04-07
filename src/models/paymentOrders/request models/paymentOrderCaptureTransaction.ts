affd32f34cd479f35e21d5d008d1d5c953c8baec
Xillians <chkxillians@gmail.com>
2022-04-07 12:44:05 +0200

    validate Capture transaction

--- src/models/paymentOrders/request models/paymentOrderCaptureTransaction.ts
+++ src/models/paymentOrders/request models/paymentOrderCaptureTransaction.ts
@@ -1,10 +1,24 @@
 import { OrderItem } from "../../../Services/PaymentOrder/models/OrderItem";
 import { Amount } from "../../generics/amount";
+import { BaseModel } from "../../generics/baseModel";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface PaymentOrderCaptureTransaction {
+export class PaymentOrderCaptureTransaction extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => Amount)
     amount: Amount;
+
+    @v.IsString()
     description: string;
+
+    @v.IsArray()
+    @v.ValidateNested()
+    @Type(() => OrderItem)
     orderItems: OrderItem[];
+    @v.IsString()
     payeeReference: string;
+    @v.ValidateNested()
+    @Type(() => Amount)
     vatAmount: Amount;
 }
