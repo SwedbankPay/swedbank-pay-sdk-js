30e58ff3015a63b74abd324ce9651154f88c798e
Xillians <chkxillians@gmail.com>
2022-04-07 12:16:12 +0200

    validate vipps capture request

--- src/models/paymentInstruments/vipps/vippsPaymentCaptureRequest.ts
+++ src/models/paymentInstruments/vipps/vippsPaymentCaptureRequest.ts
@@ -1,5 +1,10 @@
+import { BaseModel } from "../../generics/baseModel";
 import { CaptureTransaction } from "../transactions/captureTransaction";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface VippsPaymentCaptureRequest {
+export class VippsPaymentCaptureRequest extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => CaptureTransaction)
     transaction: CaptureTransaction;
 }
