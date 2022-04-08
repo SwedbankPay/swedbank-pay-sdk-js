77037e203c8454258163c49562e5c66f8ea9738e
Xillians <chkxillians@gmail.com>
2022-04-08 11:12:39 +0200

    validated paymentorder request details

--- src/models/paymentOrders/request models/paymentOrderRequestDetails.ts
+++ src/models/paymentOrders/request models/paymentOrderRequestDetails.ts
@@ -9,22 +9,67 @@ import { Operation } from "../../enums/operation";
 import { Urls } from "../../generics/urls";
 import { Payer } from "../payer";
 import { PaymentOrderPaymentOptionsItems } from "../paymentOrderPaymentOptionsItems";
+import { BaseModel } from "../../generics/baseModel";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface PaymentOrderRequestDetails {
+export class PaymentOrderRequestDetails extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => Amount)
     amount: Amount;
+
+    @v.ValidateNested()
+    @Type(() => Currency)
     currency: Currency;
+
+    @v.IsString()
     description: string;
+    
+    @v.IsBoolean()
     generateRecurrenceToken: boolean;
+
+    @v.IsArray()
+    @v.ValidateNested()
+    @Type(() => PaymentOrderPaymentOptionsItems)
     items: PaymentOrderPaymentOptionsItems[];
+
+    @v.ValidateNested()
+    @v.ValidateNested()
     language: Language;
+
+    @v.ValidateNested()
+    @Type(() => MetaData)
     metadata: MetaData;
+
+    @v.IsEnum(Operation)
     operation: Operation;
+
+    @v.ValidateNested()
+    @Type(() => OrderItem)
     orderItems: OrderItem[];
+
+    @v.ValidateNested()
+    @Type(() => PayeeInfo)
     payeeInfo: PayeeInfo;
+
+    @v.ValidateNested()
+    @Type(() => Payer)
     payer: Payer;
+
+    @v.ValidateNested()
+    @Type(() => RiskIndicator)
     riskIndicator: RiskIndicator;
+
+    @v.IsUrl()
     urls: Urls;
+
+    @v.IsString()
     userAgent: string;
+
+    @v.ValidateNested()
+    @Type(() => Amount)
     vatAmount: Amount;
-    disablePaymentMenu: boolean | null;
+
+    @v.IsBoolean()
+    disablePaymentMenu?: boolean;
 }
