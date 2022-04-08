095e88535e05744b24351103a95f3fe1f48f63ea
Xillians <chkxillians@gmail.com>
2022-04-08 11:12:22 +0200

    validated paymentorder items

--- src/models/paymentOrders/paymentOrderPaymentOptionsItems.ts
+++ src/models/paymentOrders/paymentOrderPaymentOptionsItems.ts
@@ -1,9 +1,20 @@
+import { BaseModel } from "../generics/baseModel";
 import { CreditCardOptions } from "../creditCardOptions";
 import { Invoice } from "./invoice";
 import { Swish } from "./swish";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface PaymentOrderPaymentOptionsItems {
+export class PaymentOrderPaymentOptionsItems extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => CreditCardOptions)
     creditCard: CreditCardOptions;
+
+    @v.ValidateNested()
+    @Type(() => Invoice)
     invoice: Invoice;
+
+    @v.ValidateNested()
+    @Type(() => Swish)
     swish: Swish;
 }
