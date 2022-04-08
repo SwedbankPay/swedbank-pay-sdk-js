90047b8c8e62127e82584e7d3fb0f15ae8630314
Xillians <chkxillians@gmail.com>
2022-04-08 11:12:04 +0200

    validated invoice item

--- src/models/paymentOrders/invoice.ts
+++ src/models/paymentOrders/invoice.ts
@@ -1,3 +1,7 @@
-export interface Invoice {
-    feeAmount: number | null;
+import * as v from 'class-validator';
+import { BaseModel } from "../generics/baseModel";
+
+export class Invoice extends BaseModel{
+    @v.IsString()
+    feeAmount?: number;
 }
