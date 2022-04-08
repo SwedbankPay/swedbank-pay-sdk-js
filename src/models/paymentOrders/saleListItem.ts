a7407b9cebf6cbb0afc59e672444462b2f0b800e
Xillians <chkxillians@gmail.com>
2022-04-08 12:08:37 +0200

    sale list item validated

--- src/models/paymentOrders/saleListItem.ts
+++ src/models/paymentOrders/saleListItem.ts
@@ -1,16 +1,40 @@
 import { Amount } from "../generics/amount";
 import { PaymentType } from "../paymentInstruments/enums/paymentType";
 import { State } from "../enums/state";
+import { BaseModel } from "../generics/baseModel";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface SaleListItem {
+export class SaleListItem extends BaseModel{
+    @v.IsUrl()
     id: URL;
+
+    @v.IsString()
     created: string;
+
+    @v.IsString()
     updated: string;
+
+    @v.IsEnum(PaymentType)
     type: PaymentType;
+
+    @v.IsEnum(State)
     state: State;
+
+    @v.IsNumber()
     number: number;
+
+    @v.ValidateNested()
+    @Type(() => Amount)
     amount: Amount;
+
+    @v.ValidateNested()
+    @Type(() => Amount)
     vatAmount: Amount;
+
+    @v.IsString()
     description: string;
+
+    @v.IsString()
     payeeReference: string;
 }
