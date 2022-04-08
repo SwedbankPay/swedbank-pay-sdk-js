3b7920e7580449a0c0a89fa9006acfbb85b17eff
Xillians <chkxillians@gmail.com>
2022-04-08 11:11:39 +0200

    add validator to paymentorder payee info

--- src/Services/PaymentOrder/models/PayeeInfo.d.ts
+++ src/Services/PaymentOrder/models/PayeeInfo.d.ts
@@ -1,18 +1,24 @@
-export interface PayeeInfo {
+import * as v from 'class-validator';
+import { BaseModel } from "../../../models/generics/baseModel";
+
+export class PayeeInfo extends BaseModel{
   /**
    * The payeeId, this is usually the Merchant ID (you). Found by accessing
    * the Admin Portal.
    */
+  @v.IsString()
   payeeId: string;
   /**
    * A operation unique reference for your system. It is set per operation
    * to ensure an exactly-once delivery of a transactional operation.
    * Must conform to the regex ^[A-Za-z0-9]{1,12}
    */
+  @v.IsString()
   payeeReference: string;
   /**
    * Used to display the name of the payee/merchant in the payment frame.
    */
+  @v.IsString()
   payeeName?: string;
   /**
    * A product category or number sent in from the payee.
@@ -20,12 +26,14 @@ export interface PayeeInfo {
    * 50, but will be passed through the
    * payment process and may be used in the settlement process.
    */
+  @v.IsString()
   productCategory?: string;
   /**
    * The order reference should reflect the order reference found your systems.
    * Can be used to track an order in your system.
    * Max length of 50 characters.
    */
+  @v.IsString()
   orderReference?: string;
   /**
    * The subsite field can be used to perform split settlement on the payment.
@@ -34,5 +42,6 @@ export interface PayeeInfo {
    * Max length of 40 characters.
    * Read more: https://developer.swedbankpay.com/payments/card/other-features#split-settlement
    */
+  @v.IsString()
   subsite?: string;
 }
