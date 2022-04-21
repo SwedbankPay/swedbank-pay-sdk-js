6e5a3ba3863f96b89a2046109512d96efb089cdf
Xillians <chkxillians@gmail.com>
2022-04-21 10:59:44 +0200

    validated payment order currenth payment response

--- src/models/paymentOrders/response models/currentPaymentResponse.ts
+++ src/models/paymentOrders/response models/currentPaymentResponse.ts
@@ -1,7 +1,17 @@
+import { BaseModel } from "../../generics/baseModel";
 import { CurrentPayment } from "../currentPayment";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface CurrentPaymentResponse {
+export class CurrentPaymentResponse extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => URL)
     paymentOrder: URL;
+
+    @v.IsString()
     menuElementName: string;
+
+    @v.ValidateNested()
+    @Type(() => CurrentPayment)
     payment: CurrentPayment;
 }
