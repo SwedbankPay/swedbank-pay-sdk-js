4e69a0407a16d0613975329b1d41b0d9989e20c8
Xillians <chkxillians@gmail.com>
2022-04-21 10:58:08 +0200

    validated payment order update request details

--- src/models/paymentOrders/request models/paymentOrderUpdateRequestDetails.ts
+++ src/models/paymentOrders/request models/paymentOrderUpdateRequestDetails.ts
@@ -1,10 +1,23 @@
 import { OrderItem } from "../../../Services/PaymentOrder/models/OrderItem";
 import { Amount } from "../../generics/amount";
 import { Operation } from "../../enums/operation";
+import { BaseModel } from "../../generics/baseModel";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface PaymentOrderUpdateRequestDetails {
+export class PaymentOrderUpdateRequestDetails extends BaseModel{
+    @v.ValidateNested()
+    @Type(() => Amount)
     amount: Amount;
+
+    @v.IsEnum(Operation)
     operation: Operation;
+    
+    @v.ValidateNested()
+    @Type(() => Amount)
     vatAmount: Amount;
+    
+    @v.ValidateNested()
+    @Type(() => OrderItem)
     orderItems: OrderItem[];
 }
