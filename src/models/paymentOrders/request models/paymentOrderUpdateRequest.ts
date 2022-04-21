ffae42d439a145f3c2953b4ef3277b9a1aa490b6
Xillians <chkxillians@gmail.com>
2022-04-21 10:58:21 +0200

    validated payment order update request

--- src/models/paymentOrders/request models/paymentOrderUpdateRequest.ts
+++ src/models/paymentOrders/request models/paymentOrderUpdateRequest.ts
@@ -1,5 +1,11 @@
+import { BaseModel } from "../../generics/baseModel";
 import { PaymentOrderUpdateRequestDetails } from "./paymentOrderUpdateRequestDetails";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface PaymentOrderUpdateRequest {
+export class PaymentOrderUpdateRequest extends BaseModel {
+
+    @v.ValidateNested()
+    @Type(() => PaymentOrderUpdateRequestDetails)
     paymentOrder: PaymentOrderUpdateRequestDetails;
 }
