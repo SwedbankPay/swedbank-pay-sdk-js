d83a326cd00ee96b2a82e0461453b65ad62a5628
Xillians <chkxillians@gmail.com>
2022-04-21 10:57:48 +0200

    validated payment order list response

--- src/models/paymentOrders/response models/paymentAuthorizationListResponse.ts
+++ src/models/paymentOrders/response models/paymentAuthorizationListResponse.ts
@@ -1,6 +1,10 @@
+import { Identifiable } from "../../generics/identifiable";
 import { PaymentAuthorization } from "../../paymentInstruments/paymentAuthorization";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface PaymentAuthorizationListResponse {
-    id: URL;
+export class PaymentAuthorizationListResponse extends Identifiable{
+    @v.ValidateNested()
+    @Type(() =>PaymentAuthorization)
     authorizationList: PaymentAuthorization[];
 }
