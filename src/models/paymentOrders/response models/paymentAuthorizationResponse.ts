f6276344608b42b074e4cd46a73cc3e1a113941c
Xillians <chkxillians@gmail.com>
2022-04-21 10:57:36 +0200

    validated payment order authorization response

--- src/models/paymentOrders/response models/paymentAuthorizationResponse.ts
+++ src/models/paymentOrders/response models/paymentAuthorizationResponse.ts
@@ -2,15 +2,37 @@ import { Amount } from "../../generics/amount";
 import { Identifiable } from "../../generics/identifiable";
 import { PaymentType } from "../../paymentInstruments/enums/paymentType";
 import { State } from "../../enums/state";
+import * as v from 'class-validator';
+import { Type } from "class-transformer";
 
-export interface PaymentAuthorizationResponse extends Identifiable {
+export class PaymentAuthorizationResponse extends Identifiable {
+
+    @v.IsString()
     created: string;
+    
+    @v.IsString()
     updated: string;
+
+    @v.IsEnum(PaymentType)
     type: PaymentType;
+
+    @v.IsEnum(State)
     state: State;
+
+    @v.IsNumber()
     number: number;
+
+    @v.ValidateNested()
+    @Type(() =>Amount)
     amount: Amount;
+
+    @v.ValidateNested()
+    @Type(() =>Amount)
     vatAmount: Amount;
+    
+    @v.IsString()
     description: string;
+    
+    @v.IsString()
     payeeReference: string;
 }
