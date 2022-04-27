2d1e3d51ee1740335fe72016525ce07f53208bcd
Xillians <sandra.toftemo@payex.com>
2022-04-27 13:17:52 +0200

    Lint declare before use

--- src/models/paymentInstruments/invoice/approvedLegalAddressRequest.ts
+++ src/models/paymentInstruments/invoice/approvedLegalAddressRequest.ts
@@ -2,16 +2,16 @@ import * as v from 'class-validator';
 import { Type } from 'class-transformer';
 import { BaseModel } from '../../generics/baseModel';
 
-export class ApprovedLegalAddressRequest extends BaseModel {
-    @v.ValidateNested()
-    @Type(() => LegalAddress)
-    addressee: LegalAddress;
-}
-
 class LegalAddress extends BaseModel{
     @v.IsString()
     socialSecurityNumber: string;
 
     @v.IsString()
     zipCode: string;
+}
+
+export class ApprovedLegalAddressRequest extends BaseModel {
+    @v.ValidateNested()
+    @Type(() => LegalAddress)
+    addressee: LegalAddress;
 }
