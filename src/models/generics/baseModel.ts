f71d4567e037ca8d33ea18f215d56d154de18639
Xillians <sandra.toftemo@payex.com>
2022-04-27 13:17:40 +0200

    lint return type

--- src/models/generics/baseModel.ts
+++ src/models/generics/baseModel.ts
@@ -2,7 +2,7 @@ import * as v from 'class-validator';
 import { plainToInstance } from 'class-transformer';
 
 export class BaseModel {
-    static make(data: unknown) {
+    static make(data: unknown): BaseModel {
       const asset = plainToInstance(BaseModel, data);
       if (!v.validate(asset)) {
         throw new Error('invalid data.');
