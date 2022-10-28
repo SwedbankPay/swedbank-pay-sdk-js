30254f92adade10d8c84e4c675c45b11622942ea
Xillians <chkxillians@gmail.com>
2022-10-28 13:59:35 +0200

    renaming legal address & abort request

--- src/models/paymentInstruments/invoice/approvedLegalAddressResponse.ts
+++ src/models/paymentInstruments/invoice/approvedLegalAddressResponse.ts
@@ -1,7 +1,7 @@
 import { Type } from 'class-transformer';
 import * as v from 'class-validator';
 import { Identifiable } from '../../generics/identifiable';
-import { LegalAddress } from '../transactions/legalAddress';
+import { LegalAddress } from '../transactions/legal-address';
 
 export class ApprovedLegalAddressResponse extends Identifiable{
     @v.ValidateNested()
--- src/models/paymentInstruments/transactions/paymentAbortRequest.ts
+++ src/models/paymentInstruments/transactions/paymentAbortRequest.ts
@@ -1,7 +1,7 @@
 import { Type } from 'class-transformer';
 import * as v from 'class-validator'
 import { BaseModel } from '../../generics/baseModel';
-import { PaymentAbortRequestDetails } from './paymentAbortRequestDetails';
+import { PaymentAbortRequestDetails } from './payment-abort-request-details';
 
 export class PaymentAbortRequest extends BaseModel{
     @Type(() => PaymentAbortRequest)
