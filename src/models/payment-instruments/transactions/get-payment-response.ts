ea9c861a07be93c38f938dc0f61bbce7994deb7c
Xillians <chkxillians@gmail.com>
2022-10-28 14:55:06 +0200

    formatting

--- src/models/payment-instruments/transactions/get-payment-response.ts
+++ src/models/payment-instruments/transactions/get-payment-response.ts
@@ -17,75 +17,75 @@ import { SaleListResponse } from './sale-list-response';
 import { TransactionListResponse } from './transaction-list-response';
 
 export class GetPaymentResponse extends Identifiable {
-    @v.IsNumber()
-    number: number;
+  @v.IsNumber()
+  number: number;
 
-    @v.IsString()
-    created: string;
+  @v.IsString()
+  created: string;
 
-    @v.IsString()
-    updated: string;
+  @v.IsString()
+  updated: string;
 
-    @v.IsString()
-    instrument: string;
+  @v.IsString()
+  instrument: string;
 
-    @v.IsEnum(Operation)
-    operation: Operation;
+  @v.IsEnum(Operation)
+  operation: Operation;
 
-    @v.IsEnum(PaymentIntent)
-    intent: PaymentIntent;
+  @v.IsEnum(PaymentIntent)
+  intent: PaymentIntent;
 
-    @v.IsEnum(State)
-    state: State;
+  @v.IsEnum(State)
+  state: State;
 
-    @v.ValidateNested()
-    @Type(() => Currency)
-    currency: Currency;
+  @v.ValidateNested()
+  @Type(() => Currency)
+  currency: Currency;
 
-    @v.IsArray()
-    @v.ValidateNested()
-    @Type(() => Price)
-    prices: Price[];
-    amount: Amount;
+  @v.IsArray()
+  @v.ValidateNested()
+  @Type(() => Price)
+  prices: Price[];
+  amount: Amount;
 
-    @v.IsString()
-    description: string;
+  @v.IsString()
+  description: string;
 
-    @v.IsString()
-    initiatingSystemUserAgent: string;
+  @v.IsString()
+  initiatingSystemUserAgent: string;
 
-    @v.IsString()
-    userAgent: string;
+  @v.IsString()
+  userAgent: string;
 
-    @v.ValidateNested()
-    @Type(() => Language)
-    language: Language;
+  @v.ValidateNested()
+  @Type(() => Language)
+  language: Language;
 
-    @v.ValidateNested()
-    @Type(() => TransactionListResponse)
-    transactions: TransactionListResponse;
+  @v.ValidateNested()
+  @Type(() => TransactionListResponse)
+  transactions: TransactionListResponse;
 
-    @v.ValidateNested()
-    @Type(() => AuthorizationListResponse)
-    authorizations?: AuthorizationListResponse;
+  @v.ValidateNested()
+  @Type(() => AuthorizationListResponse)
+  authorizations?: AuthorizationListResponse;
 
-    @v.ValidateNested()
-    @Type(() => SaleListResponse)
-    sales?: SaleListResponse;
+  @v.ValidateNested()
+  @Type(() => SaleListResponse)
+  sales?: SaleListResponse;
 
-    @v.ValidateNested()
-    @Type(() => CancellationListResponse)
-    cancellations?: CancellationListResponse;
+  @v.ValidateNested()
+  @Type(() => CancellationListResponse)
+  cancellations?: CancellationListResponse;
 
-    @v.IsArray()
-    @v.IsUrl()
-    urls: Urls[];
+  @v.IsArray()
+  @v.IsUrl()
+  urls: Urls[];
 
-    @v.ValidateNested()
-    @Type(() => PayeeInfo)
-    payeeInfo: PayeeInfo;
+  @v.ValidateNested()
+  @Type(() => PayeeInfo)
+  payeeInfo: PayeeInfo;
 
-    @v.ValidateNested()
-    @Type(() => MetaData)
-    metadata: MetaData;
+  @v.ValidateNested()
+  @Type(() => MetaData)
+  metadata: MetaData;
 }
